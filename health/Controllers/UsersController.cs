9422d7255485bed7eaca88e35be4f42d8121008a
Artur Belak <a.belak@temabit.com>
2022-09-24 22:56:21 +0300

    CORS

--- health/Controllers/UsersController.cs
+++ health/Controllers/UsersController.cs
@@ -21,51 +21,7 @@ namespace health.Controllers
         {
             _logger = logger;
         }
-        //public Users user = new Users()
-        //{
-        //    Id = "d54ed580-a5e6-4f75-bfd7-ed2d29ffd6ca",
-        //    BirthDate = "1970-01-01",
-        //    FirstName = "",
-        //    Gender = "cccc",
-        //    LastName = "sss"
-        //};
-
-        static string contentResp = @"{
-  'users': [
-    {
-      'id': '392d86f8-2c81-48bc-9ca6-848862cc4afe',
-      'firstName': 'Mask',
-      'lastName': 'Bond',
-      'gender': 'Male',
-      'birthDate': '1970-01-01'
-    },
-    {
-
-      'id': '37b33826-5aa5-4fe0-96d9-42c65a43e067',
-      'firstName': 'Ahtiom',
-      'lastName': 'Belka',
-      'gender': 'Male',
-      'birthDate': '1970-01-01'
-    },
-    {
-      'id': 'd54ed580-a5e6-4f75-bfd7-ed2d29ffd6ca',
-      'firstName': 'Olgich',
-      'lastName': 'Svyshch',
-      'gender': 'Female',
-      'birthDate': '1970-01-01'
-    },
-    {
-      'id': '32aeb91e-7663-4b5f-9e73-54af9aa3e70f',
-      'firstName': 'Katsya',
-      'lastName': 'Logoyda',
-      'gender': 'Female',
-      'birthDate': '1970-01-01'
-    }
-
-  ]
-}";
-
-        GetUsers respClass = JsonConvert.DeserializeObject<GetUsers>(contentResp);
+       
         [HttpGet(Name = "GetUsers")]
         public GetUsers Get()
         {
